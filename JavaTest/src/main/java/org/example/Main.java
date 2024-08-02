e32383f89fa69eb6bef6db8dbb9e1555e0889dfc
RAR27 <romulo.antonio_rodrigues@hotmail.com>
2024-08-02 11:55:53 -0300

    Teste 3

--- JavaTest/src/main/java/org/example/Main.java
+++ JavaTest/src/main/java/org/example/Main.java
@@ -3,7 +3,9 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hello and welcome!");
+        System.out.println("Hello and welcome!");
+
+        //TESTE
 
     }
 }
